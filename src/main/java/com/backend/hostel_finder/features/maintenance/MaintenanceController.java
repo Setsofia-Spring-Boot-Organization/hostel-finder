ccc09398ac7cdceb5b933676f768e627313dafe4
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-08-18 23:38:52 +0000

    Update

--- src/main/java/com/backend/hostel_finder/features/maintenance/MaintenanceController.java
+++ src/main/java/com/backend/hostel_finder/features/maintenance/MaintenanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hf/api/maintenance")
+@RequestMapping("/hf/api/v1/maintenance")
 @RequiredArgsConstructor
 public class MaintenanceController {
 
