2ce4e56300021698162ae87e5652b82576f1a112
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-08-19 01:56:11 +0000

    Update

--- src/main/java/com/backend/hostel_finder/features/booking/daos/CreateBookingRequest.java
+++ src/main/java/com/backend/hostel_finder/features/booking/daos/CreateBookingRequest.java
@@ -1,33 +1,10 @@
 package com.backend.hostel_finder.features.booking.daos;
 
 import com.backend.hostel_finder.features.booking.BookingDocument;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 
 import java.time.LocalDateTime;
-
-@Data
-public class CreateBookingRequest {
-
-    private String studentId;
-
-    private String roomId;
-
-    private String hostelName;
-
-    private String roomType;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime checkInDate;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime checkOutDate;
-
-    private Double amountPaid;
-
-    private String paymentReference;
-}
 
 @Data
 public class CreateBookingRequest {
